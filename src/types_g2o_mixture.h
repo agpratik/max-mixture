f02bb1cd711aa487d122a587f20cf7ff0c771158
Pratik Agarwal <agarwal@informatik.uni-freiburg.de>
2012-10-02 10:49:28 +0200

    added disclaimer

--- src/types_g2o_mixture.h
+++ src/types_g2o_mixture.h
@@ -1,3 +1,28 @@
+// Max-Mixture plugin for g2o
+// Copyright (C) 2012 P. Agarwal, E. Olson, W. Burgard
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are
+// met:
+//
+// * Redistributions of source code must retain the above copyright notice,
+//   this list of conditions and the following disclaimer.
+// * Redistributions in binary form must reproduce the above copyright
+//   notice, this list of conditions and the following disclaimer in the
+//   documentation and/or other materials provided with the distribution.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
+// IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+// TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+// PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+// HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+// SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
+// TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+// PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+// LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+// NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+// SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 #ifndef G2O_TYPES_MIXTURE_
 #define G2O_TYPES_MIXTURE_
@@ -6,9 +31,6 @@
 #include "g2o/stuff/macros.h"
 
 #include "edge_se2_mixture.h"
-
-//using namespace g2o;
-//I dont care if it is not microsoft
-//void init_types_mixture();
-//G2O_REGISTER_TYPE("EDGE_SE2_MIXTURE", EdgeSE2Mixture);
+#include "edge_se2_pointxy_mixture.h"
+#include "edge_se3_mixture.h"
 #endif
